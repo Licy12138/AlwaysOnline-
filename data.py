de9b8feb264578ee8975be25fe1559565f6e361d
Licy12138 <165685209+Licy12138@users.noreply.github.com>
2024-09-19 13:01:47 +0800

    Update data.py

--- data.py
+++ data.py
@@ -2,6 +2,13 @@
 # These example values won't work. You must get your own api_id and
 # api_hash from https://my.telegram.org, under API Development.
 
-api_id = 123456  # Your ID here | 请替换为你的id
-api_hash = 'Your Token Here | 请填写API_Hash'
-delay = 15  # Second
+# 要使用 Telethon 连接到 Telegram，您需要在 Telegram 的 API Development 页面获取自己的 api_id 和 api_hash。以下是如何设置的步骤：
+# 注册 Telegram 应用，访问 https://my.telegram.org。
+# 登录您的 Telegram 帐户。
+# 选择 "API Development Tools"。
+# 创建一个新应用，填写所需信息。
+# 获得 api_id 和 api_hash，填入到下方
+
+api_id = 'your_api_id'
+api_hash = 'your_api_hash'
+delay = 15  #每秒检测在线状态，默认30，单位秒
